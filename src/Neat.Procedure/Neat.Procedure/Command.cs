9ee4a4778c3fad2429c01d9bc627a711503ed5a3
Alberto Chvaicer <achvaicer@gmail.com>
2013-02-19 17:17:46 -0300

    retrieving store procedure parameters name

--- src/Neat.Procedure/Neat.Procedure/Command.cs
+++ src/Neat.Procedure/Neat.Procedure/Command.cs
@@ -6,6 +6,9 @@ namespace Neat.Procedure
 {
     internal class Command
     {
+        private const string ParametersQuery = "SELECT PARAMETER_NAME FROM INFORMATION_SCHEMA.Parameters WHERE SPECIFIC_NAME = @SpecificName ORDER BY ORDINAL_POSITION ASC";
+        private const string ParameterNameColumn = "PARAMETER_NAME";
+
         internal static int? CommandTimeout { get; set; }
 
         internal static SqlParameter AddReturnValueAsParameter(SqlCommand cmd)
@@ -23,11 +26,11 @@ namespace Neat.Procedure
 
         internal static void Prepare(SqlCommand cmd, SqlTransaction trans, string storeProcedureName, params object[] parameters)
         {
-            ArgumentsToParameters(cmd, parameters);
             cmd.Transaction = trans;
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.CommandText = storeProcedureName;
             cmd.CommandTimeout = CommandTimeout ?? 30;
+            ArgumentsToParameters(cmd, parameters);
         }
 
         private static void DictionaryToParameters(SqlCommand cmd, Dictionary<string, object> parameters)
@@ -41,8 +44,19 @@ namespace Neat.Procedure
         private static void ArgumentsToParameters(SqlCommand cmd, params object[] parameters)
         {
             if (parameters == null || parameters.Length == 0) return;
-            foreach (var parameter in parameters)
-                cmd.Parameters.Add(parameter);
+            using (var connection = new SqlConnection(cmd.Connection.ConnectionString))
+            {
+                connection.Open();
+                using (var command = new SqlCommand(ParametersQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@SpecificName", cmd.CommandText);
+                    var reader = command.ExecuteReader();
+                    var i = 0;
+                    while (reader.Read() && i < parameters.Length)
+                        cmd.Parameters.AddWithValue(reader[ParameterNameColumn] as string, parameters[i++]);
+                }
+                connection.Close();
+            }
         }
     }
 }
